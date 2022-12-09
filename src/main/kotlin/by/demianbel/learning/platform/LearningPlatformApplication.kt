1a9651ee1ed4703cabf51940f2f42b0f80faec09
Demian Belsky <demianbel@gmail.com>
2022-12-09 22:06:17 +0300

    create spring boot application

---
+++ src/main/kotlin/by/demianbel/learning/platform/LearningPlatformApplication.kt
@@ -0,0 +1,11 @@
+package by.demianbel.learning.platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningPlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/by/demianbel/learning/platform/LearningPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.demianbel.learning.platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningPlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
